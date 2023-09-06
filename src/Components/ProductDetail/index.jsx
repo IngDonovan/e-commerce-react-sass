8fc9e0b6871f46f3328dbcb17f7e981f9b2710f6
Donovan Rojas <rojasdrm03@gmail.com>
2023-09-05 21:26:34 -0500

    fix detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,45 +1,36 @@
-import { useContext } from 'react';
-import { EcomContext } from '../../Context';
+import { useContext } from "react";
+import { EcomContext } from "../../Context";
 import { AiOutlineClose } from "react-icons/Ai";
-import './ProductDetail.scss';
+import "./ProductDetail.scss";
 
 const ProductDetail = () => {
-    const { 
-        isProductDetailOpen, 
-        toggleProductDetail,
-        productToShow,
-    } = useContext(EcomContext);
+  const { isProductDetailOpen, toggleProductDetail, productToShow } =
+    useContext(EcomContext);
 
-    return(
-        <aside className={isProductDetailOpen ? "detailAside" : "hidden"}>
-            <div className='detailHeader'>
-                <h2>
-                    Detail
-                </h2>
-                <span className="closeIcoSpan"
-                onClick={toggleProductDetail}>
-                    <AiOutlineClose 
-                    className='xIco'/>
-                </span>
-            </div>
-            <div className='detailContent'>
-                <figure className=''>
-                    <img className=''
-                    src={productToShow.image}
-                    alt={productToShow.title}/>
-                </figure>
-                <div className=''>
-                    <p className="dtPrice">${productToShow.price}</p>
-                    <span className="dtTitle">
-                        {productToShow.title}
-                    </span>
-                    <span className='dtDescription'>
-                        {productToShow.description}
-                    </span>
-                </div>
-            </div>
-        </aside>
-    );
+  return (
+    <aside className={isProductDetailOpen ? "detailAside" : "hidden"}>
+      <div className="detailHeader">
+        <h2>Detail</h2>
+        <span className="closeIcoSpan" onClick={toggleProductDetail}>
+          <AiOutlineClose className="xIco" />
+        </span>
+      </div>
+      <div className="detailContent">
+        <figure className="">
+          <img
+            className=""
+            src={productToShow.image}
+            alt={productToShow.title}
+          />
+        </figure>
+        <div className="">
+          <p className="dtPrice">${productToShow.price}</p>
+          <span className="dtTitle">{productToShow.title}</span>
+          <span className="dtDescription">{productToShow.description}</span>
+        </div>
+      </div>
+    </aside>
+  );
 };
 
 export default ProductDetail;
