ef6a666f78c7bfeb56e421a69fc1c3b636d1e622
Donovan Rojas <rojasdrm03@gmail.com>
2023-12-05 21:12:03 -0500

    set localstorage to account and sign-out

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -57,6 +57,28 @@ const EcomProvider = ({ children }) => {
     }
   };
 
+  const initializeLocalStorage = () => {
+    const accountInLocalStorage = localStorage.getItem('account');
+    const signOutInLocalStorage = localStorage.getItem('sign-out');
+    let parsedAccount;
+    let parsedSignOut;
+
+    if (!accountInLocalStorage) {
+      localStorage.setItem('account', JSON.stringify({}));
+      parsedAccount = {};
+    } else {
+      parsedAccount = JSON.parse(accountInLocalStorage);
+    }
+
+    if (!signOutInLocalStorage) {
+      localStorage.setItem('sign-out', JSON.stringify(false));
+      parsedSignOut = false;
+    } else {
+      parsedSignOut = JSON.parse(signOutInLocalStorage);
+    }
+
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       try {
