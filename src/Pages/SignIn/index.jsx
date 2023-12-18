5c84abfc7b675359633e554b94d1fdf20c7185da
Donovan Rojas <rojasdrm03@gmail.com>
2023-12-18 09:00:35 -0500

    has an account in localStorage

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -8,7 +8,18 @@ const SignIn = () => {
     isCardFlipped,
     setIsCardFlipped,
     handleCardFlip,
+    account,
   } = useContext(EcomContext);
+
+  //Account
+  const accountInLcl = localStorage.getItem('account');
+  const parsedAccount = JSON.parse(accountInLcl);
+  //Has ann account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true;
+  const noAccountInLocalState = account ? Object.keys(account).length === 0 : true;
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
+  
+
   return (
     <Layout>
       
